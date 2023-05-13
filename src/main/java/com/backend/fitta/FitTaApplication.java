ac5453f03d73c6075674ad6afadef74d638c34de
sunjun <hgfdsa4320@naver.com>
2023-05-13 15:16:43 +0900

    초기 설정

---
+++ src/main/java/com/backend/fitta/FitTaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.fitta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTaApplication.class, args);
+	}
+
+}
