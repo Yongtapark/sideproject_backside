2e7b1471439b41297ef34debfb38228ec1f7040a
sunjun <hgfdsa4320@naver.com>
2023-05-13 15:16:43 +0900

    초기 설정

---
+++ src/test/java/com/backend/fitta/FitTaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.fitta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitTaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
