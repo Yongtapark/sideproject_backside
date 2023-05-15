d221ce70a2f356493d02a0595eb4df718a25a871
Yong Ta Park <qkrdydxk80@nate.com>
2023-05-16 00:56:10 +0900

    add : CustomOAuth2UserService, OAuthAttributes, SecurityConfig, SessionUser

--- src/main/java/com/backend/fitta/config/oauth/SecurityConfig.java
+++ src/main/java/com/backend/fitta/config/oauth/SecurityConfig.java
@@ -1,10 +1,9 @@
-package com.backend.fitta.config.aouth;
+package com.backend.fitta.config.oauth;
 
 
 import com.backend.fitta.entity.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
