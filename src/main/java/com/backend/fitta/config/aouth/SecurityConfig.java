9775e90a9d655a5189c88b51372cc0caf91653f5
Yong Ta Park <qkrdydxk80@nate.com>
2023-05-15 11:23:16 +0900

    update : CustomOAuth2UserService.java

--- src/main/java/com/backend/fitta/config/aouth/SecurityConfig.java
+++ src/main/java/com/backend/fitta/config/aouth/SecurityConfig.java
@@ -1,25 +1,36 @@
 package com.backend.fitta.config.aouth;
 
 
+import com.backend.fitta.entity.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityBuilder;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain,HttpSecurity> {
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+@RequiredArgsConstructor
+public class SecurityConfig{
+    private final CustomOAuth2UserService customOAuth2UserService;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
                 .authorizeRequests()
+                .requestMatchers("/","/css/**","/images/**","/js/**").permitAll()
+                .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
+                .logout()
+                .logoutSuccessUrl("/")
                 .and()
-                .httpBasic();
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+        return http.build();
+
     }
 }
