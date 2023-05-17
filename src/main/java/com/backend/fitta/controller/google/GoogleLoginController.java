89b2ce9e249526bbb1398f6497085a1cc7e10a02
Yong Ta Park <qkrdydxk80@nate.com>
2023-05-17 11:48:59 +0900

    add : GoogleLoginController.java update : SecurityConfig.java 구글 로그인 버튼 클릭하여 로그인정보 db 저장 성공

--- src/main/java/com/backend/fitta/controller/google/GoogleLoginController.java
+++ src/main/java/com/backend/fitta/controller/google/GoogleLoginController.java
@@ -1,16 +1,35 @@
 package com.backend.fitta.controller.google;
 
+import com.backend.fitta.config.oauth.CustomOAuth2UserService;
+import com.backend.fitta.config.oauth.dto.OAuthAttributes;
+import com.backend.fitta.config.oauth.dto.SessionUser;
+import com.backend.fitta.entity.user.User;
+import com.backend.fitta.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class GoogleLoginController {
+    private final HttpSession httpSession;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final UserRepository userRepository;
 
-   /* @GetMapping
-    public ResponseEntity<Void> getGoogleAuthUrl(){
+   @GetMapping("/api/user/login")
+    public SessionUser login(@RequestBody OAuthAttributes attributes){
+       User user = saveOrUpdate(attributes);
+       httpSession.setAttribute("user",new SessionUser(user));
+       return new SessionUser(user);
+   }
 
-    }*/
+    private User saveOrUpdate(OAuthAttributes attributes){
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+        return userRepository.save(user);
+    }
 }
