1140829f97a2bb28b4ce0366a9c770874f4b3746
sunjun <hgfdsa4320@naver.com>
2023-05-18 11:05:43 +0900

    update MemberController

--- src/main/java/com/backend/fitta/controller/member/MemberController.java
+++ src/main/java/com/backend/fitta/controller/member/MemberController.java
@@ -3,7 +3,6 @@ package com.backend.fitta.controller.member;
 import com.backend.fitta.dto.Member.FindByEmailResponse;
 import com.backend.fitta.dto.Member.SignUpRequest;
 import com.backend.fitta.dto.Member.UpdateMemberRequest;
-import com.backend.fitta.entity.member.Member;
 import com.backend.fitta.exception.MemberNotFoundException;
 import com.backend.fitta.service.member.MemberService;
 import jakarta.validation.Valid;
@@ -12,8 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,9 +22,6 @@ public class MemberController {
 
     @PostMapping
     public ResponseEntity<Long> saveMember(@Valid @RequestBody SignUpRequest request) {
-        log.info("id={}",request.getEmail());
-        log.info("pw={}",request.getPassword());
-
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.save(request));
     }
 
