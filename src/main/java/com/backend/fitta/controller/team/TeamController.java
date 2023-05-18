1e6fd5538b5329f7a4b8aa16ac216d714884a62e
sunjun <hgfdsa4320@naver.com>
2023-05-18 15:23:46 +0900

    add Team Entity CRUD

---
+++ src/main/java/com/backend/fitta/controller/team/TeamController.java
@@ -0,0 +1,55 @@
+package com.backend.fitta.controller.team;
+
+import com.backend.fitta.dto.Member.FindByEmailResponse;
+import com.backend.fitta.dto.Member.UpdateMemberRequest;
+import com.backend.fitta.dto.team.FindTeamByIdResponse;
+import com.backend.fitta.dto.team.SaveTeamRequest;
+import com.backend.fitta.dto.team.UpdateTeamRequest;
+import com.backend.fitta.exception.MemberNotFoundException;
+import com.backend.fitta.exception.TeamNotFoundException;
+import com.backend.fitta.service.interfaces.TeamService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/team")
+public class TeamController {
+    private final TeamService teamService;
+
+    @PostMapping
+    public ResponseEntity<Long> saveTeam(@Valid @RequestBody SaveTeamRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(teamService.save(request));
+    }
+
+    @GetMapping("/{teamId}")
+    public ResponseEntity<FindTeamByIdResponse> findTeam(@PathVariable long teamId) {
+        validateExistTeam(teamId);
+        return ResponseEntity.ok(teamService.findById(teamId).get());
+    }
+
+    @PutMapping("/{teamId}")
+    public ResponseEntity<Long> updateTeam(@PathVariable Long teamId, @Valid @RequestBody UpdateTeamRequest request) {
+        validateExistTeam(teamId);
+        return ResponseEntity.ok(teamService.updateTeam(teamId, request));
+    }
+
+
+    @DeleteMapping("/{teamId}")
+    public ResponseEntity<Void> deleteMember(@PathVariable Long teamId) {
+        validateExistTeam(teamId);
+        teamService.deleteTeam(teamId);
+        return ResponseEntity.noContent().build();
+    }
+
+    private void validateExistTeam(Long teamId) {
+        teamService.findById(teamId).orElseThrow(() -> new TeamNotFoundException());
+    }
+
+
+}
