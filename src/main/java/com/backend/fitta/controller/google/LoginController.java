0f18aea870d964028cffa46ac7c662a4c94ca36b
Yong Ta Park <qkrdydxk80@nate.com>
2023-05-19 18:21:26 +0900

    update properties : spring.security.oauth2.client.provider.google.token-uri=https://oauth2.googleapis.com/token update properties : custom.resource-uri=https://www.googleapis.com/auth/userinfo.profile
    add : AccountInfo.java
    add : LoginController.java
    add : LoginService.java
    
    update : SecurityConfig.java

---
+++ src/main/java/com/backend/fitta/controller/google/LoginController.java
@@ -0,0 +1,63 @@
+package com.backend.fitta.controller.google;
+
+
+import com.backend.fitta.dto.google.AccountInfo;
+import com.backend.fitta.service.LoginService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+@RestController
+@RequestMapping(produces = "application/json")
+@RequiredArgsConstructor
+@Slf4j
+public class LoginController {
+    private final LoginService loginService;
+    private final WebClient webClient=WebClient.builder().baseUrl("http://localhost:8081").build();
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String redirectUri;
+    private String loginPage;
+
+    /**
+     * 구글 로그인 페이지로 이동합니다
+     * @param response
+     * @throws IOException
+     */
+    @GetMapping("/v1/signin")
+    public void googleLogin(HttpServletResponse response) throws IOException {
+        encodeLoginPage(response);
+    }
+
+    /**
+     * 구글에서 보내주는 값을 받아 AccountInfo 객체로 변환합니다.
+     * @param code
+     * @param registrationId
+     * @return
+     */
+    @GetMapping("/login/oauth2/code/{registrationId}")
+    public AccountInfo login(@RequestParam String code, @PathVariable String registrationId) {
+        AccountInfo accountInfo = loginService.socialLogin(code, registrationId);
+        log.info("userInfo={}",accountInfo);
+        return accountInfo;
+    }
+
+
+    private void encodeLoginPage(HttpServletResponse response) throws IOException {
+        String scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
+        String encodedScope = URLEncoder.encode(scope, "UTF-8");
+        String encodeRedirectUrl = URLEncoder.encode(redirectUri, "UTF-8");
+        loginPage="https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id="+clientId+"&redirect_uri="+encodeRedirectUrl+"&response_type=code&scope="+encodedScope;
+
+        response.sendRedirect(loginPage);
+    }
+}
