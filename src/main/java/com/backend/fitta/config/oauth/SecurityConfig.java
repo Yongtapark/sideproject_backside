4aec33fb832c330bf2a1188a8f6841101616ef0e
Yong Ta Park <qkrdydxk80@nate.com>
2023-05-20 14:42:15 +0900

    add : Urls.java update : SecurityConfig.java update : WebConfig.java update : LoginController.java

--- src/main/java/com/backend/fitta/config/oauth/SecurityConfig.java
+++ src/main/java/com/backend/fitta/config/oauth/SecurityConfig.java
@@ -15,6 +15,15 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity // 스프링 시큐리티 설정 활성화
 @RequiredArgsConstructor
@@ -23,38 +32,47 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig{
     private final CustomOAuth2UserService customOAuth2UserService;
     private final JwtTokenProvider jwtTokenProvider;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        List<RequestMatcher> permitAllRequestMatchers = Arrays.asList(
+                new AntPathRequestMatcher("/v1/userinfo"),
+                new AntPathRequestMatcher("/members/login"),
+                new AntPathRequestMatcher("/oauth2/authorization/google"),
+                new AntPathRequestMatcher("/auth/sign"),
+                new AntPathRequestMatcher("https://fitta-git-dev-yiminwook.vercel.app/**"),
+                new AntPathRequestMatcher("/login/oauth2/code/{registrationId}")
+        );
 
-      /*  http
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(new OrRequestMatcher(permitAllRequestMatchers)).permitAll()
+                                .anyRequest().authenticated()
+                )
                 .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers("/members/login","/api/info").permitAll()
-                .requestMatchers("/members/test").hasRole("USER")
-                .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-                .oauth2Login()
-                .defaultSuccessUrl("/api/user/login")
-                .userInfoEndpoint()
-                .userService(customOAuth2UserService);*/
-        http
-                .httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .requestMatchers("/v1/userinfo","/members/login","/oauth2/authorization/google").permitAll()
-                .anyRequest().permitAll()
-                .and()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);// JWT 인증 필터를 추가
+                .cors().configurationSource(corsConfigurationSource());
+
         return http.build();
     }
 
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://fitta-git-dev-yiminwook.vercel.app")); // 여기에 허용하려는 도메인을 추가
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token", "access-control-allow-credentials", "access-control-allow-origin"));
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
