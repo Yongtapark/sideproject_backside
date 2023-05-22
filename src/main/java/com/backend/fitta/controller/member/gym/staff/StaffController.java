56cd4d3174a22328d1f93439a1f3b3164ddc6265
sunjun <hgfdsa4320@naver.com>
2023-05-23 00:39:06 +0900

    add api descriptions

--- src/main/java/com/backend/fitta/controller/member/gym/staff/StaffController.java
+++ src/main/java/com/backend/fitta/controller/member/gym/staff/StaffController.java
@@ -3,14 +3,23 @@ package com.backend.fitta.controller.member.gym.staff;
 import com.backend.fitta.dto.team.FindStaffByIdResponse;
 import com.backend.fitta.dto.team.SaveStaffRequest;
 import com.backend.fitta.dto.team.UpdateStaffRequest;
+import com.backend.fitta.exception.StaffNotFoundException;
 import com.backend.fitta.service.apiService.interfaces.StaffApiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.bridge.MessageUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "스태프", description = "스태프 관련 api 입니다.")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -19,22 +28,25 @@ public class StaffController {
 
     private final StaffApiService staffApiService;
 
+    @Operation(summary = "스태프 등록 메서드", description = "스태프 등록 메서드입니다.")
     @PostMapping
     public ResponseEntity<Long> saveStaff(@Valid @RequestBody SaveStaffRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(staffApiService.save(request));
     }
 
+    @Operation(summary = "스태프 조회 메서드", description = "스태프 id로 스태프를 조회 할 수 있습니다.")
     @GetMapping("/{staffId}")
     public ResponseEntity<FindStaffByIdResponse> findStaff(@PathVariable long staffId) {
         return ResponseEntity.ok(staffApiService.findById(staffId));
     }
 
+    @Operation(summary = "스태프 정보 수정 메서드", description = "스태프 id로 스태프를 찾아 스태프의 정보를 수정 할 수 있습니다.")
     @PutMapping("/{staffId}")
     public ResponseEntity<Long> updateTeam(@PathVariable Long staffId, @Valid @RequestBody UpdateStaffRequest request) {
         return ResponseEntity.ok(staffApiService.update(staffId, request));
     }
 
-
+    @Operation(summary = "스태프 삭제 메서드", description = "스태프 id로 스태프를 삭제할 수 있습니다.")
     @DeleteMapping("/{staffId}")
     public ResponseEntity<Void> deleteTeam(@PathVariable Long staffId) {
         staffApiService.delete(staffId);
--- src/main/java/com/backend/fitta/controller/member/gym/team/TeamController.java
+++ src/main/java/com/backend/fitta/controller/member/gym/team/TeamController.java
@@ -4,6 +4,8 @@ import com.backend.fitta.dto.team.FindTeamByIdResponse;
 import com.backend.fitta.dto.team.SaveTeamRequest;
 import com.backend.fitta.dto.team.UpdateTeamRequest;
 import com.backend.fitta.service.apiService.interfaces.TeamApiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "팀", description = "팀 관련 api 입니다.")
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -19,22 +22,25 @@ public class TeamController {
 
     private final TeamApiService teamApiService;
 
+    @Operation(summary = "팀 등록 메서드", description = "팀 등록 메서드입니다.")
     @PostMapping
     public ResponseEntity<Long> saveTeam(@Valid @RequestBody SaveTeamRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(teamApiService.save(request));
     }
 
+    @Operation(summary = "팀 조회 메서드", description = "팀 id로 팀 정보를 조회 할 수 있습니다.")
     @GetMapping("/{teamId}")
     public ResponseEntity<FindTeamByIdResponse> findTeam(@PathVariable long teamId) {
         return ResponseEntity.ok(teamApiService.findById(teamId));
     }
 
+    @Operation(summary = "팀 정보 수정 메서드", description = "팀 id로 팀 정보를 찾아 팀의 정보를 수정 할 수 있습니다.")
     @PutMapping("/{teamId}")
     public ResponseEntity<Long> updateTeam(@PathVariable Long teamId, @Valid @RequestBody UpdateTeamRequest request) {
         return ResponseEntity.ok(teamApiService.updateTeam(teamId, request));
     }
 
-
+    @Operation(summary = "팀 삭제 메서드", description = "팀 id로 팀을 삭제할 수 있습니다.")
     @DeleteMapping("/{teamId}")
     public ResponseEntity<Void> deleteTeam(@PathVariable Long teamId) {
         teamApiService.deleteTeam(teamId);
