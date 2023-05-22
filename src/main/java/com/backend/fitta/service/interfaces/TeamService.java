c23e0bb6cef6f0ff1d6f90a7dd266c9ada88c7e0
Yong Ta Park <qkrdydxk80@nate.com>
2023-05-22 14:11:06 +0900

    merging...

---
+++ src/main/java/com/backend/fitta/service/interfaces/TeamService.java
@@ -0,0 +1,19 @@
+package com.backend.fitta.service.interfaces;
+
+import com.backend.fitta.dto.team.FindTeamByIdResponse;
+import com.backend.fitta.dto.team.SaveTeamRequest;
+import com.backend.fitta.dto.team.UpdateTeamRequest;
+import com.backend.fitta.entity.gym.Staff;
+import com.backend.fitta.entity.gym.Team;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public interface TeamService {
+    Long save(SaveTeamRequest request);
+    Optional<FindTeamByIdResponse> findById(Long id);
+    List<Team> findAll();
+    Long updateTeam(Long id, UpdateTeamRequest request);
+    void deleteTeam(Long id);
+}
