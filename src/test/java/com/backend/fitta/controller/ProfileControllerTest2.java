012036bf512bec21270dbf3bedaa101240397247
Yong Ta Park <qkrdydxk80@nate.com>
2023-05-26 20:08:23 +0900

    add : ProfileControllerTest2.java add : ProfileControllerTest.java add : ProfileController.java

---
+++ src/test/java/com/backend/fitta/controller/ProfileControllerTest2.java
@@ -0,0 +1,36 @@
+package com.backend.fitta.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
+class ProfileControllerTest2 {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void profile은_인증없이_호출된다() throws Exception{
+        String expected = "test";
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("/profile", String.class);
+        log.info("forEntity={}",forEntity);
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+
+    }
+
+
+}
