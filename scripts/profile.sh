0417373194b1f15f1699da3444765265375fb1fc
Yong Ta Park <qkrdydxk80@nate.com>
2023-05-28 00:54:22 +0900

    fix : 스크립트 파일 오류 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -6,8 +6,8 @@ function find_idle_profile() {
   #현재 엔진엑스가 바라보고 있는 스프링부트가 정상적으로 수행중인지 확인
   #응답값을 HttpStatus로 받는다.
   #정상이면 200,오류가 발생한다면 400~503 사이로 발생하니, 400 이상은 모두 예외로 보고 real2를 현재 profile로 사용한다.
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"
-                                http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w
+    "%{http_code}" http://localhost:${IDLE_PORT}/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] #400보다 크면(즉, 40x/50x 에러 모두 포함)
     then
