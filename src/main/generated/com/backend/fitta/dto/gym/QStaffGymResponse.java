6cb08eb81674fa9b7a6b99ba3c0b5479b250f9b8
Yong Ta Park <qkrdydxk80@nate.com>
2023-05-27 15:28:40 +0900

    fix : 스크립트 파일 오류 수정

---
+++ src/main/generated/com/backend/fitta/dto/gym/QStaffGymResponse.java
@@ -0,0 +1,21 @@
+package com.backend.fitta.dto.gym;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.ConstructorExpression;
+import javax.annotation.processing.Generated;
+
+/**
+ * com.backend.fitta.dto.gym.QStaffGymResponse is a Querydsl Projection type for StaffGymResponse
+ */
+@Generated("com.querydsl.codegen.DefaultProjectionSerializer")
+public class QStaffGymResponse extends ConstructorExpression<StaffGymResponse> {
+
+    private static final long serialVersionUID = 824332821L;
+
+    public QStaffGymResponse(com.querydsl.core.types.Expression<String> name, com.querydsl.core.types.Expression<java.time.LocalDate> birthday, com.querydsl.core.types.Expression<com.backend.fitta.entity.enums.Gender> gender, com.querydsl.core.types.Expression<String> phoneNumber, com.querydsl.core.types.Expression<String> address, com.querydsl.core.types.Expression<com.backend.fitta.entity.enums.Grade> grade) {
+        super(StaffGymResponse.class, new Class<?>[]{String.class, java.time.LocalDate.class, com.backend.fitta.entity.enums.Gender.class, String.class, String.class, com.backend.fitta.entity.enums.Grade.class}, name, birthday, gender, phoneNumber, address, grade);
+    }
+
+}
+
