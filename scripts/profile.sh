6cb1ea765222d23f73c9a7c49a52ada20d6ca301
Yong Ta Park <qkrdydxk80@nate.com>
2023-05-27 23:49:40 +0900

    fix : 스크립트 파일 오류 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -34,10 +34,10 @@ function find_idle_profile() {
 function find_idle_port() {
     IDLE_PROFILE=$(find_idle_profile) #find_idle_profile 함수의 결과를 IDLE_PROFILE 변수에 할당
 
-    if [ ${IDLE_PROFILE} == real1 ]
+    if [ ${IDLE_PROFILE} == real ]
     then
-      echo "8082"
+      echo "8081"
     else
-      echo "8083"
+      echo "8082"
     fi
 }
