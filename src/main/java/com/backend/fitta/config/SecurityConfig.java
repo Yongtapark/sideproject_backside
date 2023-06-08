1c718689ed0a81b1d79c891e349e207c34aa18ee
Yong Ta Park <qkrdydxk80@nate.com>
2023-06-08 17:57:44 +0900

    update : 상태코드 200 추가

--- src/main/java/com/backend/fitta/config/SecurityConfig.java
+++ src/main/java/com/backend/fitta/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -67,7 +68,7 @@ public class SecurityConfig{
 
                 .logout()
                 .logoutUrl("/signout")
-                //.logoutSuccessHandler((request, response, authentication) -> response.sendRedirect("/"))
+                .logoutSuccessHandler((request, response, authentication) -> response.setStatus(200))
                 .deleteCookies("accessToken")
                 .and()
 
